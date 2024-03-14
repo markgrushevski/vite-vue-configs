dcdae182e33b90949a9b46dd568ed0e5211328d1
Leonid <68914627+markgrushevski@users.noreply.github.com>
2024-03-14 12:32:14 +0500

    refactor: 🏭improve vite.config.js, remove vite.config.ts, remove vite-plugin-mkcert, remove postcss-nested from docs install script

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,12 @@ import vue from '@vitejs/plugin-vue';
 import autoprefixer from 'autoprefixer';
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
-import mkcert from 'vite-plugin-mkcert';
 // @formatter:off
 // prettier-ignore
 export default defineConfig({
-    plugins: [ vue(), mkcert() ],
-    server: { cors: true, port: 7777, https: true },
-    preview: { cors: true, port: 7777, https: true },
+    plugins: [ vue() ],
+    server: { port: 7777 },
+    preview: { port: 7777 },
     build: { sourcemap: true },
     esbuild: { sourcemap: 'external' },
     css: { postcss: { plugins: [ autoprefixer ] }, devSourcemap: true },
@@ -16,10 +15,7 @@ export default defineConfig({
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
             '@app': fileURLToPath(new URL('./src/app', import.meta.url)),
-            '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
-            '@widgets': fileURLToPath(new URL('./src/widgets', import.meta.url)),
-            '@features': fileURLToPath(new URL('./src/features', import.meta.url)),
-            '@entities': fileURLToPath(new URL('./src/entities', import.meta.url)),
+            '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
             '@shared': fileURLToPath(new URL('./src/shared', import.meta.url))
         }
     }
