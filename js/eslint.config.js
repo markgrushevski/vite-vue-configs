07d997cd88ecd5efecf0c66e39432172ef8c7151
Leonid <68914627+markgrushevski@users.noreply.github.com>
2024-04-19 15:45:25 +0500

    feat: 🎂update eslint config to v9, add and remove some packages for it; update husky setup, npm scripts, readme file

---
+++ js/eslint.config.js
@@ -0,0 +1,17 @@
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import pluginVue from 'eslint-plugin-vue';
+import globals from 'globals';
+
+export default /** @type { import('eslint').Linter.FlatConfig[] } */ ([
+    {
+        files: ['src/**/*.{js,vue}'],
+        languageOptions: {
+            globals: globals.browser,
+            parserOptions: { ecmaVersion: 'latest', sourceType: 'module' }
+        }
+    },
+    pluginJs.configs.recommended,
+    ...pluginVue.configs['flat/recommended'],
+    eslintConfigPrettier
+]);
---
+++ ts/eslint.config.js
@@ -0,0 +1,19 @@
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import pluginVue from 'eslint-plugin-vue';
+import globals from 'globals';
+import tsESLint from 'typescript-eslint';
+
+export default /** @type { import('eslint').Linter.FlatConfig[] } */ ([
+    {
+        files: ['src/**/*.{ts,vue}'],
+        languageOptions: {
+            globals: globals.browser,
+            parserOptions: { ecmaVersion: 'latest', sourceType: 'module' }
+        }
+    },
+    pluginJs.configs.recommended,
+    ...tsESLint.configs.recommended,
+    ...pluginVue.configs['flat/recommended'],
+    eslintConfigPrettier
+]);
